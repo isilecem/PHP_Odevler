422133e3b15502344cb5351879422ecd07c7ad42
isilecem <eceemozel@gmail.com>
2018-03-21 19:50:51 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Öğrenci Yönetim Sistemi</title>
+</head>
+<body>
+<form action="kontrol.php" method="POST" name="login">
+<label>Öğrenci no:</label><input type="text" name="ogrno"><br>
+<label>Şifre:</label><input type="password" name="sifre">
+<button type="submit">GÖNDER</button><br>
+<label><i>Öğrenci numaranız "2018123456@..." şeklinde 10 haneli sayıdan oluşmalıdır!</i></label>
+</form>
+</body>
+</html>
